02048b8e6df2f94179c4cd608145b7c918afad09
Itsuki-2822 <135577168+Itsuki-2822@users.noreply.github.com>
2024-06-17 18:49:54 +0900

    Create update_util.sh

---
+++ util/update_util.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 現在のディレクトリを保存
+CURRENT_DIR=$(pwd)
+
+# temp_cloneディレクトリの作成と移動
+mkdir -p ../temp_clone
+cd ../temp_clone
+
+# リポジトリの初期化とリモート追加
+git init
+git remote add origin https://github.com/Itsuki-2822/ML_Utilities.git
+
+# スパースチェックアウトの設定とpull
+git sparse-checkout init --cone
+git sparse-checkout set util
+git pull origin main
+
+# utilフォルダをコピー
+cp -r util "$CURRENT_DIR/"
+
+# 一時ディレクトリの削除
+cd "$CURRENT_DIR"
+rm -rf ../temp_clone
+
+# 更新内容のコミットとプッシュ
+git add util
+git commit -m "Updated util folder from https://github.com/Itsuki-2822/ML_Utilities.git repository"
+git push origin main
+
+echo "util directory has been updated and changes are pushed."
